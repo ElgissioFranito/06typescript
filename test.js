e179400b13edd0c0ead1a8cc6d44bb24dbe5b150
ElgissioFranito <mistera.inwy@gmail.com>
2022-07-07 08:17:19 +0300

    TS de Simon Dieny

--- test.js
+++ test.js
@@ -1,4 +1,34 @@
-var hello = function (nom) {
-    console.log("salut " + nom + " !");
-};
-hello("Rakoto");
+// *********  installation typescript  ************* //
+// npm install -g typescript
+// *********  voir la version ***************** //
+// tsc -v
+// *********  compiler un fichier .ts *********** //
+// tsc nom_du_fichier.ts
+// *********  choisir un cible de sortie ******** // (par defaut ES3)
+// tsc nom_du_fichier.ts --target ES5
+// HelloWorld 
+function miarahaba(olona) {
+    return console.log("miarahaba anao izahay " + olona);
+}
+miarahaba('Elgissio');
+// Declarer une variable => nomVariable : typeVariable = valeur;
+var ninona; // any : nimport_quel_type
+var anarana = 'Superman'; // string : type chaine de caractere
+var pointDeVie = 1000; // number : type nombre
+var misidina = true; // boolean : type booléen
+// // ma propre type variable (ex: Mafibe)
+// var ianao: Mafibe = new Mafibe(anarana, pointDeVie, misidina);
+// // les fonctions
+// function creatHeros(anarana: string, pointDeVie?: number): Mafibe { // le type du return
+//     var unHeros = new Mafibe();                  // Mafibe : notre propre type (que nous n'avons pas enocre créer)
+//     unHeros.anarana = anarana;
+//     if(pointDeVie) {                             // pointDeVie? (avec un ? dans la parametre de la fonction) est un parametre facultatif
+//        unHeros.pointDeVie = pointDeVie;
+//     }
+//     return unHeros;                             // unHeros est de type Mafibe
+// }
+var Mafibe = /** @class */ (function () {
+    function Mafibe() {
+    }
+    return Mafibe;
+}());
--- test.ts
+++ test.ts
@@ -1,5 +1,53 @@
-let hello = (nom) => {
-    console.log("salut " + nom + " !");
-}
+// *********  installation typescript  ************* //
+// npm install -g typescript
 
-hello("Rakoto");
+// *********  voir la version ***************** //
+// tsc -v
+
+// *********  compiler un fichier .ts *********** //
+// tsc nom_du_fichier.ts
+
+// *********  choisir un cible de sortie ******** // (par defaut ES3)
+// tsc nom_du_fichier.ts --target ES5
+
+
+
+
+// HelloWorld 
+function miarahaba(olona) {
+    return console.log("miarahaba anao izahay " + olona);
+}
+miarahaba('Elgissio');
+
+
+// Declarer une variable => nomVariable : typeVariable = valeur;
+var ninona: any;                            // any : nimport_quel_type
+
+var anarana: string = 'Superman';           // string : type chaine de caractere
+
+var pointDeVie: number = 1000;              // number : type nombre
+
+var misidina: boolean = true;               // boolean : type booléen
+
+
+
+// // ma propre type variable (ex: Mafibe)
+// var ianao: Mafibe = new Mafibe(anarana, pointDeVie, misidina);
+
+
+
+// // les fonctions
+// function creatHeros(anarana: string, pointDeVie?: number): Mafibe { // le type du return
+//     var unHeros = new Mafibe();                  // Mafibe : notre propre type (que nous n'avons pas enocre créer)
+//     unHeros.anarana = anarana;
+
+//     if(pointDeVie) {                             // pointDeVie? (avec un ? dans la parametre de la fonction) est un parametre facultatif
+//        unHeros.pointDeVie = pointDeVie;
+//     }
+
+//     return unHeros;                             // unHeros est de type Mafibe
+// }
+
+class Mafibe {
+
+}
